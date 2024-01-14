1ae5639bbda8cf7801f055239da5054144f7d462
Prachi-2002 <prachimadame01@gmail.com>
2024-01-14 19:48:12 +0530

    fixed the naming convention

--- src/helpers/getMovieList.js
+++ src/helpers/getMovieList.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getMovieList = async (val ='batman', page=1) => {
-    const response = await axios.get(`http://www.omdbapi.com/?apiKey=${process.env.apiKey}&s=${val}&page=${page}`);
+    const response = await axios.get(`http://www.omdbapi.com/?apiKey=${process.env.REACT_APP_OMDB_API_KEY}&s=${val}&page=${page}`);
     console.log(response, 'Response ');
     const obj = {
         result: response.data.Search,
