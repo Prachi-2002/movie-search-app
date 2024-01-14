abb2b042c24dbd8bdbf742e511b53938196c1043
Prachi-2002 <prachimadame01@gmail.com>
2024-01-14 21:08:32 +0530

    fixed the issue with helper func

--- src/helpers/getMovieList.js
+++ src/helpers/getMovieList.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const getMovieList = async (val ='batman', page=1) => {
-    const response = await axios.get(`http://www.omdbapi.com/?apiKey=${process.env.REACT_APP_OMDB_API_KEY}&s=${val}&page=${page}`);
-    console.log(response, 'Response ');
+    const response = await axios.get(`http://www.omdbapi.com/?apiKey=ff24d901&s=${val}&page=${page}`,
+    );
     const obj = {
         result: response.data.Search,
         total: response.data.totalResults
